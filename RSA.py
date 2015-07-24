946f3fb338c8433709af1deff6278ae380e07bfd
Adrian Anton <adanto64@gmail.com>
2015-07-24 20:07:20 +0200

    Added totient function an some tests. All passed

--- RSA.py
+++ RSA.py
@@ -16,6 +16,15 @@
 from numpy import random
 from math import sqrt
 
+# Totient:
+# Function that counts the positive integers less than n that are 
+# relatively primes to n (coprimes)
+def totient(n):
+	return len([i for i in xrange(1, n) if coprimes(i, n)]) if n > 1 else 1
+
+
+# Coprimes: 
+# Two numbers are coprimes where the only divisor in common is 1
 def coprimes(a, b):
 	aDiv = [1]
 	for i in xrange(2, a/2 + 1):
@@ -27,9 +36,7 @@ def coprimes(a, b):
 		if b % i == 0:
 			bDiv.append(i)
 	bDiv.append(b)
-
-	print a, aDiv
-	print b, bDiv
+	return list(set(aDiv).intersection(set(bDiv))) == [1]
 
 def isPrime(num):
 	if num % 2 == 0:
@@ -72,10 +79,14 @@ def mainRSA(digits = 16):
 
 
 def main():
+	#print coprimes(3,5)
 	test(16)
-
+	#mainRSA()
 
 def test(digits = 16):
+	print "---------- TEST TOTIENT ----------"
+	testTotient()
+	print "------- TEST TOTIENT ENDED -------\n"
 	print "---------- TEST COPRIMES ----------"
 	testCoprimes()
 	print "------- TEST COPRIMES ENDED -------\n"
@@ -86,8 +97,39 @@ def test(digits = 16):
 	testDigitsLen(digits)
 	print "------- TEST DIG LEN ENDED -------\n"
 	
+
+
+def testTotient():
+	print "CORRECT" if totient(1) == 1 else "INCORRECT", "TOTIENT"
+	print "CORRECT" if totient(2) == 1 else "INCORRECT", "TOTIENT"
+	print "CORRECT" if totient(3) == 2 else "INCORRECT", "TOTIENT"
+	print "CORRECT" if totient(5) == 4 else "INCORRECT", "TOTIENT"
+	print "CORRECT" if totient(9) == 6 else "INCORRECT", "TOTIENT"
+	print "CORRECT" if totient(10) == 4 else "INCORRECT", "TOTIENT"
+	print "CORRECT" if totient(11) == 10 else "INCORRECT", "TOTIENT"
+	print "CORRECT" if totient(14) == 6 else "INCORRECT", "TOTIENT"
+	print "CORRECT" if totient(19) == 18 else "INCORRECT", "TOTIENT"
+	print "CORRECT" if totient(21) == 12 else "INCORRECT", "TOTIENT"
+	print "CORRECT" if totient(24) == 8 else "INCORRECT", "TOTIENT"
+	print "CORRECT" if totient(29) == 28 else "INCORRECT", "TOTIENT"
+	print "CORRECT" if totient(30) == 8 else "INCORRECT", "TOTIENT"
+	print "CORRECT" if totient(40) == 16 else "INCORRECT", "TOTIENT"
+	print "CORRECT" if totient(45) == 24 else "INCORRECT", "TOTIENT"
+	print "CORRECT" if totient(53) == 52 else "INCORRECT", "TOTIENT"
+	print "CORRECT" if totient(56) == 24 else "INCORRECT", "TOTIENT"
+	print "CORRECT" if totient(57) == 36 else "INCORRECT", "TOTIENT"
+	print "CORRECT" if totient(80) == 32 else "INCORRECT", "TOTIENT"
+	print "CORRECT" if totient(97) == 96 else "INCORRECT", "TOTIENT"
+	print "CORRECT" if totient(99) ====  60 else "INCORRECT", "TOTIENT"
+
 def testCoprimes():
-	print "CORRECT" if coprimes(10, 5) == True else "INCORRECT"
+	print "CORRECT" if coprimes(14, 15) == True else "INCORRECT"
+	print "CORRECT" if coprimes(14, 21) == False else "INCORRECT"
+	print "CORRECT" if coprimes(3, 5) == True else "INCORRECT"
+	print "CORRECT" if coprimes(10, 5) == False else "INCORRECT"
+	print "CORRECT" if coprimes(10, 9) == True else "INCORRECT"
+	print "CORRECT" if coprimes(9, 9) == False else "INCORRECT"
+	print "CORRECT" if coprimes(10, 11) == True else "INCORRECT"
 
 def testDigits(digits):
 	for _ in xrange(5):
